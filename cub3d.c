d8bb65c3242780be2c55eb40c53d4f11e07cc765
martimsaunders <martimprazeres13@gmail.com>
2025-10-20 16:53:17 +0100

    testes

--- cub3d.c
+++ cub3d.c
@@ -6,49 +6,105 @@
 /*   By: praders <praders@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 12:10:34 by praders           #+#    #+#             */
-/*   Updated: 2025/10/20 13:39:52 by praders          ###   ########.fr       */
+/*   Updated: 2025/10/20 16:52:57 by praders          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int end_window(void *param)
+t_game *pc(void)
 {
-    t_map *game;
+    static t_game pc;
 
-    game = (t_map *)param;
-    mlx_destroy_window(game->mlx, game->win);
-    mlx_destroy_display(game->mlx);
-    free(game->mlx);
+    return (&pc);
+}
+
+int end_window(void)
+{
+    mlx_destroy_window(pc()->mlx, pc()->win);
+    mlx_destroy_display(pc()->mlx);
+    free(pc()->mlx);
     exit(0);
     return (0);
 }
 
-int handle_input(int keycode, void *param)
+int handle_input(int keycode)
 {
-    t_map *game;
-
-    game = (t_map *)param;
     if (keycode == XK_Escape)
     {
-        mlx_destroy_window(game->mlx, game->win);
-        mlx_destroy_display(game->mlx);
-        free(game->mlx);
+        mlx_destroy_window(pc()->mlx, pc()->win);
+        mlx_destroy_display(pc()->mlx);
+        free(pc()->mlx);
         exit(0);
     }
     return (0);
 }
 
+void create_map_player()
+{
+    int temp[6][6] = {
+        {1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 0, 1},
+        {1, 0, 0, 1, 0, 1},
+        {1, 0, 1, 0, 0, 1},
+        {1, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1}
+    };
+    ft_memcpy(pc()->map, temp, sizeof(temp));
+    pc()->player.x = 2.5;
+    pc()->player.y = 2.5;
+    pc()->player.dir_x = -1.0;
+    pc()->player.dir_y = 0.0;
+    pc()->player.plane_x = 0.0;
+    pc()->player.plane_y = 0.66;
+    pc()->player.move_speed = 0.05;
+    pc()->player.rot_speed = 0.03;
+}
+
+void put_pixel(int x, int y, int color)
+{
+    int offset;
+
+    if (x >= 0 && x < LENGHT && y >= 0 && y < WIDTH)
+    {
+        offset = (y * pc()->image.line_lenght) + (x * pc()->image.bpp /8);
+        *(unsigned int *)(pc()->image.addr + offset) = color;
+    }
+}
+
+int render_frame(void)
+{
+    int x;
+    int y;
+
+    y = 0;
+    while (y < WIDTH)
+    {
+        x = 0;
+        while (x < LENGHT)
+        {
+            if (y < WIDTH / 2)
+                put_pixel(x, y, 0x87CEEB);
+            else
+                put_pixel(x, y, 0x228B22);
+            x++;
+        }
+        y++;
+    }
+    mlx_put_image_to_window(pc()->mlx, pc()->win, pc()->image.image, 0, 0);
+    return (0);
+}
+
 int main(void)
 {
-    t_map test;
-
-    ft_memset(&test, 0, sizeof(t_map));
-    test.mlx = mlx_init();
-    test.win = mlx_new_window(test.mlx, LENGHT, WIDTH, "cub3d");
-    mlx_hook(test.win, 2, 1L << 0, handle_input, &test);
-    mlx_hook(test.win, 17, 0, end_window, &test);
-    mlx_loop(test.mlx);
-    
+    pc()->mlx = mlx_init();
+    pc()->win = mlx_new_window(pc()->mlx, LENGHT, WIDTH, "cub3d");
+    pc()->image.image = mlx_new_image(pc()->mlx, LENGHT, WIDTH);
+    pc()->image.addr = mlx_get_data_addr(pc()->image.image, &pc()->image.bpp, &pc()->image.line_lenght, &pc()->image.endian);
+    create_map_player();
+    mlx_hook(pc()->win, 2, 1L << 0, handle_input, NULL);
+    mlx_hook(pc()->win, 17, 0, end_window, NULL);
+    mlx_loop_hook(pc()->mlx, render_frame, NULL);
+    mlx_loop(pc()->mlx);
     return (0);
 }
--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: praders <praders@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 12:07:57 by praders           #+#    #+#             */
-/*   Updated: 2025/10/20 13:27:20 by praders          ###   ########.fr       */
+/*   Updated: 2025/10/20 16:39:56 by praders          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,46 @@
 #define LENGHT 1280
 #define WIDTH 720
 
-typedef struct s_map
+typedef struct s_enemy
 {
-    void    *mlx;
-    void    *win;
-    int     x;
-    int     y;
-}   t_map;
+    float x;
+    float y;
+    float min_x;
+    float min_y;
+    float speed;
+    int direction;
+}   t_enemy;
+
+typedef struct s_player
+{
+    float x;
+    float y;
+    float dir_x;
+    float dir_y;
+    float plane_x;
+    float plane_y;
+    float move_speed;
+    float rot_speed;
+}   t_player;
+
+typedef  struct s_image
+{
+    void *image;
+    char *addr;
+    int bpp;
+    int line_lenght;
+    int endian;
+} t_image;
+
+typedef struct s_game
+{
+    void *mlx;
+    void *win;
+    int map[6][6];
+    int enemy_count;
+    t_enemy *enemies;
+    t_player player;
+    t_image image;
+} t_game;
 
 #endif
